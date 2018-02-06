d8f74a3ee01632771e15b73184ce3da1f2b19789
avdean <34350184+avdean@users.noreply.github.com>
2018-02-06 23:23:16 +0100

    Update init.js

--- js/init.js
+++ js/init.js
@@ -22,7 +22,11 @@ $(document).ready(function(){
  $(document).ready(function(){
     $('.carousel').carousel();
   });
-    
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+         
 
 $('.dropdown-button').dropdown({
 		 inDuration: 300,
