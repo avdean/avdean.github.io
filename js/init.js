6a26ac83dd394ef0d9eaca3d6a6f37d1017068e0
avdean <34350184+avdean@users.noreply.github.com>
2018-02-07 11:33:43 +0100

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -8,42 +8,12 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 
 $(document).ready(function(){
-	$('.materialboxed').materialbox();
-});
-
-  $(document).ready(function(){
+		$('.materialboxed').materialbox();
     $('.tooltipped').tooltip({delay: 50});
-  });
-
-$(document).ready(function(){
-	$('.slider').slider();
-});
-
- $(document).ready(function(){
+		$('.slider').slider();
     $('.carousel').carousel();
-  });
-
-$(document).ready(function(){
     $('.sidenav').sidenav();
-  });
-         
-$(document).ready(function(){
-$('.dropdown-button').dropdown({
-		 inDuration: 300,
-		 outDuration: 225,
-		 constrainWidth: false, // Does not change width of dropdown to that of the activator
-		 hover: true, // Activate on hover
-		 gutter: 0, // Spacing from edge
-		 belowOrigin: true, // Displays dropdown below the button
-		 alignment: 'left', // Displays dropdown with edge aligned to the left of button
-		 stopPropagation: false // Stops event propagation
-	 }
- );
-// Basic Parallax and SideNav code
-
-$(document).ready(function(){
-    $('.parallax').parallax();
-  });
-
+		$('.parallax').parallax();
+		$('.dropdown-trigger').dropdown({hover: true});
 
 })(jQuery); // end of jQuery name space
