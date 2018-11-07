00b22cda73f1de5330f289964129b53e8a907ca0
Avdean <34350184+avdean@users.noreply.github.com>
2018-11-07 22:54:45 +0100

    Removed redundant sidenav JavaScript

--- assets/js/masonryextra.js
+++ assets/js/masonryextra.js
@@ -40,22 +40,6 @@
     window.sr = ScrollReveal();
     sr.reveal('.card');
 
-    // Mobile button colapse
-    function adjustIndex() {
-      $('body').scrollTop(0);
-      $("#sidenav-overlay").addClass("adjust-index");
-    }
-
-    $(".button-collapse").sideNav({
-      menuWidth: 1000, // Default is 240
-      edge: 'left', // Choose the horizontal origin
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    });
-
-    $('.button-collapse').click(function(){
-      adjustIndex();
-    });
-
     // Change sharing icon when menu is opened
     $(".click-to-toggle").click(function(){
       var icon = $(this).find(".btn-floating > i");
